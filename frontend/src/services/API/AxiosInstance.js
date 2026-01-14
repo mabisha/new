b807380ee8d714775219def2634110d9eeeeb1e6
mabdah <mabisha@telerivet.com>
2026-01-14 14:12:15 +0545

    new updates

--- frontend/src/services/API/AxiosInstance.js
+++ frontend/src/services/API/AxiosInstance.js
@@ -5,7 +5,6 @@ const axiosInstance = axios.create({
   // baseURL: "https://rosebudschoolnepal.org/app",
   // baseURL: "http://localhost:5000/app",
   withCredentials: true,
-
   headers: {
     "Content-Type": "application/json",
     // "Access-Control-Allow-Origin": "https://rosebudschoolnepal.org",
